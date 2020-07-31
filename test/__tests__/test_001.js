60d3d3fc87824669e82c7a69d0538d32c0ac4197
Mino Togna <mino.togna@gmail.com>
2020-07-31 20:04:45 +0200

    Tests: added test 02/03

--- test/__tests__/test_001.js
+++ test/__tests__/test_001.js
@@ -1,6 +1,10 @@
 const timeout = 2500
 const URL = 'http://localhost:3000'
 
+const getTextarea = async (page) => page.$('textarea')
+const getTextareaValue = async (page) => page.evaluate((element) => element.value, await getTextarea(page))
+const getCommentBox = async (page) => (await getTextarea(page)).nextSibling
+
 describe(
   '/ (CommentBox)',
   () => {
@@ -18,6 +22,25 @@ describe(
     it('01) page load', async () => {
       const main = await page.$('#main')
       expect(main).not.toBeNull()
+    })
+
+    it('02) textarea exists', async () => {
+      const textarea = await getTextarea(page)
+      expect(textarea).not.toBeNull()
+    })
+
+    it('03) type `hello how are you?` -> commentBox closed', async () => {
+      const textarea = await getTextarea(page)
+      await textarea.focus()
+
+      const text = 'hello how are you?'
+      await page.keyboard.type(text)
+
+      const textareaValue = await getTextareaValue(page)
+      expect(textareaValue).toBe(text)
+
+      const commentBox = await getCommentBox(page)
+      expect(commentBox).toBeUndefined()
     })
   },
   timeout
